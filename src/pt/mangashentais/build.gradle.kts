c37683dffcfe238d206f7677ba9c822a6b14de49
julia5454554 <julia5454554@gmail.com>
2026-08-22 00:45:38 -0300

    Update build.gradle.kts

--- src/pt/mangashentais/build.gradle.kts
+++ src/pt/mangashentais/build.gradle.kts
@@ -1,1 +1,17 @@
+import io.github.keiyoushi.gradle.api.ContentWarning
 
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "MangasHentais"
+    versionCode = 1
+    contentWarning = ContentWarning.NSFW
+    libVersion = "1.4"
+
+    source {
+        lang = "pt-BR"
+        baseUrl = "https://mangashentais.com"
+    }
+}
