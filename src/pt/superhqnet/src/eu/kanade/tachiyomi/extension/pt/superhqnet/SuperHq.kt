d790ca2fa1d9c840b59f52cc6f86b6ba7dc6e2f8
julia5454554 <julia5454554@gmail.com>
2026-08-22 16:06:43 -0300

    Update SuperHq.kt

--- src/pt/superhqnet/src/eu/kanade/tachiyomi/extension/pt/superhqnet/SuperHq.kt
+++ src/pt/superhqnet/src/eu/kanade/tachiyomi/extension/pt/superhqnet/SuperHq.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.pt.superhq
+package eu.kanade.tachiyomi.extension.pt.superhqnet
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.FilterList
