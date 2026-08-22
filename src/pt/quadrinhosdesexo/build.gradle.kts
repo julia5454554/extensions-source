b197b5061f6dc19492282235cef1f823800c8440
julia5454554 <julia5454554@gmail.com>
2026-08-22 13:30:05 -0300

    Update build.gradle.kts

--- src/pt/quadrinhosdesexo/build.gradle.kts
+++ src/pt/quadrinhosdesexo/build.gradle.kts
@@ -1,1 +1,17 @@
+import io.github.keiyoushi.gradle.api.ContentWarning
 
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "QuadrinhosDeSexo"
+    versionCode = 1
+    contentWarning = ContentWarning.NSFW
+    libVersion = "1.4"
+
+    source {
+        lang = "pt-BR"
+        baseUrl = "https://www.quadrinhosdesexo.com"
+    }
+}
