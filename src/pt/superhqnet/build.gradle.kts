1e0fc21e2d7907cc801f2fb469992f8d1bb9b44c
julia5454554 <julia5454554@gmail.com>
2026-08-22 15:43:32 -0300

    Update build.gradle.kts

--- src/pt/superhqnet/build.gradle.kts
+++ src/pt/superhqnet/build.gradle.kts
@@ -1,1 +1,17 @@
+import io.github.keiyoushi.gradle.api.ContentWarning
 
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "SuperHq"
+    versionCode = 1
+    contentWarning = ContentWarning.NSFW
+    libVersion = "1.4"
+
+    source {
+        lang = "pt-BR"
+        baseUrl = "https://www.superhq.net"
+    }
+}
