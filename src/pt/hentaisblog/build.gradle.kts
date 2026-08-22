23dd2e53a563c70bea806c4b78ef184b85e38a4d
julia5454554 <julia5454554@gmail.com>
2026-08-21 23:32:05 -0300

    Update build.gradle.kts

--- src/pt/hentaisblog/build.gradle.kts
+++ src/pt/hentaisblog/build.gradle.kts
@@ -1,1 +1,17 @@
+import io.github.keiyoushi.gradle.api.ContentWarning
 
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "HentaisBlog"
+    versionCode = 1
+    contentWarning = ContentWarning.NSFW
+    libVersion = "1.4"
+
+    source {
+        lang = "pt-BR"
+        baseUrl = "https://hentais.blog"
+    }
+}
