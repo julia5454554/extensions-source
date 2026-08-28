98a21d3474339db552a0328b87a0ffaadf5bf4be
julia5454554 <julia5454554@gmail.com>
2026-08-27 22:57:19 -0300

    Create build.gradle.kts

---
+++ src/pt/hqerotico/build.gradle.kts
@@ -0,0 +1,17 @@
+import io.github.keiyoushi.gradle.api.ContentWarning
+
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "HqErotico"
+    versionCode = 1
+    contentWarning = ContentWarning.NSFW
+    libVersion = "1.4"
+
+    source {
+        lang = "pt-BR"
+        baseUrl = "https://hqerotico.com"
+    }
+}
